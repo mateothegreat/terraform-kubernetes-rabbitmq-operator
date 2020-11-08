387947eabc308541e0374286776e18cc9f654cea
Matthew Davis <matthew@matthewdavis.io>
2020-11-08 15:53:21 -0500

    bump

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "null_resource" "kubectl" {
 
     provisioner "local-exec" {
 
-        command = "kubectl --insecure-skip-tls-verify=true --server=\"${ var.host }\" --token=\"${ var.token }\" delete -f ${ var.operator_url }; kubectl --insecure-skip-tls-verify=true --server=\"${ var.host }\" --token=\"${ var.token }\" apply -f ${ var.operator_url }
+        command = "kubectl --insecure-skip-tls-verify=true --server=\"${ var.host }\" --token=\"${ var.token }\" delete -f ${ var.operator_url }; kubectl --insecure-skip-tls-verify=true --server=\"${ var.host }\" --token=\"${ var.token }\" apply -f ${ var.operator_url }"
 
     }
 
